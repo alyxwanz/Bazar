7e72419f8b03dc4642d96e78d12a36616be5c924
alyxwanz <lay192@gmail.com>
2021-01-19 21:52:05 +0200

    add tab js logic

---
+++ src/js/menu_js/tab.js
@@ -0,0 +1,25 @@
+(function() {
+    let tabsElement = document.querySelector('.tab');
+    let previousTab;
+    
+    tabsElement.addEventListener('click', menuTab, {capture: true});
+    
+    function initFirstTab() {
+        previousTab = document.getElementById('All');
+        previousTab.classList.add('active');
+    }
+    
+    function menuTab(event) {
+        let button = event.target.closest('button');
+        if (!button) {
+            return;
+        }
+        previousTab.classList.toggle('active');
+        let attrValue = button.getAttribute('data-id');
+        let currentTabContent =  document.getElementById(attrValue);
+        currentTabContent.classList.add('active');
+        previousTab = currentTabContent;
+    }
+    
+    initFirstTab();
+})();
