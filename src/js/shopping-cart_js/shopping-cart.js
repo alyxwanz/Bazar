2ddc097112c6a753d0ac8abefceb3ff2955b377a
Vadymk95 <vadymk95@gmail.com>
2021-01-28 17:57:31 +0200

    shopping

--- src/js/shopping-cart_js/shopping-cart.js
+++
@@ -1,9 +0,0 @@
-const tBody = document.getElementById('tbody');
-const price = document.getElementById('price');
-const count = document.getElementById('count');
-
-price.innerHTML
-
-function getTotalPrice() {
-    return price * count;
-};
